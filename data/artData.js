b1c6ba4f4780305cf4e9a11b2547b629684c8fea
haohelenhu <helenhaohu@gmail.com>
2026-02-03 21:30:07 -0800

    Added gallery page

---
+++ data/artData.js
@@ -0,0 +1,50 @@
+const artworks = [
+    {
+        id: 1,
+        title: "Artwork 1",
+        imageUrl: "https://f2.toyhou.se/file/f2-toyhou-se/images/106999024_oBNy85OI2JawHBJ.png",
+        description: "this is a drawing",
+        category: "tag 1",
+        year: "2024"
+    },
+    {
+        id: 2,
+        title: "Artwork 2",
+        imageUrl: "https://f2.toyhou.se/file/f2-toyhou-se/images/106998971_BjUw9cktQ2zVLOj.png",
+        description: "this is a drawing",
+        category: "tag 1",
+        year: "2024"
+    },
+    {
+        id: 3,
+        title: "Artwork 3",
+        imageUrl: "https://f2.toyhou.se/file/f2-toyhou-se/images/103953024_HepDUBUq34CnY6C.png",
+        description: "this is a drawing",
+        category: "tag 2",
+        year: "2023"
+    },
+    {
+        id: 4,
+        title: "Artwork 4",
+        imageUrl: "https://f2.toyhou.se/file/f2-toyhou-se/images/103952645_pVrt9dOiREfzbMz.png",
+        description: "this is a drawing",
+        category: "tag 1",
+        year: "2024"
+    },
+    {
+        id: 5,
+        title: "Artwork 5",
+        imageUrl: "https://f2.toyhou.se/file/f2-toyhou-se/images/94874675_NgOSoAG0RLSAmMH.png",
+        description: "this is a drawing",
+        category: "tag 3",
+        year: "2023"
+    },
+    {
+        id: 6,
+        title: "Artwork 6",
+        imageUrl: "https://f2.toyhou.se/file/f2-toyhou-se/images/104342539_UBrQBXfa20hfwzt.png",
+        description: "this is a drawing",
+        category: "tag 3",
+        year: "2024"
+    }
+];
---
+++ data/projectData.js
@@ -0,0 +1,23 @@
+const projectsData = [
+    {
+        id: 1,
+        number: "01",
+        title: "Project 1",
+        description: "placeholder",
+        tags: ["tag 1", "tag 2", "tag 3"]
+    },
+    {
+        id: 2,
+        number: "02",
+        title: "Project 2",
+        description: "placeholder",
+        tags: ["tag 1", "tag 1", "tag 2"]
+    },
+    {
+        id: 3,
+        number: "03",
+        title: "Project 3",
+        description: "placeholder",
+        tags: ["tag 3", "tag 3", "tag 3"]
+    }
+];
