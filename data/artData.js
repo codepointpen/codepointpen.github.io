7aa535cc21e22ca05ff8006c99892c7071fe075c
haohelenhu <helenhaohu@gmail.com>
2026-02-05 01:44:35 -0800

    Updated website information

--- data/artData.js
+++ data/artData.js
@@ -2,49 +2,129 @@ const  artworksData = [
     {
         id: 1,
         title: "Artwork 1",
-        imageUrl: "images/1 heh.png",
+        imageUrl: "images/galix.png",
         description: "this is a drawing",
-        category: "tag 1",
-        year: "2024"
+        category: "misc",
+        year: "2025"
     },
     {
         id: 2,
         title: "Artwork 2",
-        imageUrl: "images/1.png",
+        imageUrl: "images/cuties.png",
         description: "this is a drawing",
-        category: "tag 1",
-        year: "2024"
+        category: "dream eater",
+        year: "2025"
     },
     {
         id: 3,
         title: "Artwork 3",
-        imageUrl: "images/pfp.png",
+        imageUrl: "images/angel nithar.png",
         description: "this is a drawing",
-        category: "tag 2",
-        year: "2023"
+        category: "lab oc",
+        year: "2025"
     },
     {
         id: 4,
         title: "Artwork 4",
-        imageUrl: "images/cosmos 1.png",
+        imageUrl: "images/anjana.png",
         description: "this is a drawing",
-        category: "tag 1",
+        category: "lab oc",
         year: "2024"
     },
     {
         id: 5,
         title: "Artwork 5",
-        imageUrl: "images/rubesty artfight.png",
+        imageUrl: "images/skylar and fayette.png",
         description: "this is a drawing",
-        category: "tag 3",
-        year: "2023"
+        category: "misc",
+        year: "2025"
     },
     {
         id: 6,
         title: "Artwork 6",
-        imageUrl: "images/transparent.png",
+        imageUrl: "images/bastian.png",
         description: "this is a drawing",
-        category: "tag 3",
+        category: "lab oc",
+        year: "2025"
+    },
+    {
+        id: 7,
+        title: "Artwork 7",
+        imageUrl: "images/maeve.png",
+        description: "this is a drawing",
+        category: "lab oc",
+        year: "2025"
+    },
+    {
+        id: 8,
+        title: "Artwork 8",
+        imageUrl: "images/justaleaf artfight.png",
+        description: "this is a drawing",
+        category: "artfight",
+        year: "2025"
+    },
+    {
+        id: 9,
+        title: "Artwork 9",
+        imageUrl: "images/nithar.png",
+        description: "this is a drawing",
+        category: "lab oc",
         year: "2024"
+    },
+    {
+        id: 10,
+        title: "Artwork 10",
+        imageUrl: "images/rubesty artfight.png",
+        description: "this is a drawing",
+        category: "artfight",
+        year: "2025"
+    },
+    {
+        id: 11,
+        title: "Artwork 11",
+        imageUrl: "images/elijah silly ref.png",
+        description: "this is a drawing",
+        category: "dnd",
+        year: "2024"
+    },
+    {
+        id: 12,
+        title: "Artwork 12",
+        imageUrl: "images/arion.png",
+        description: "this is a drawing",
+        category: "dnd",
+        year: "2024"
+    },
+    {
+        id: 13,
+        title: "Artwork 13",
+        imageUrl: "images/nithar sycthe.png",
+        description: "this is a drawing",
+        category: "lab oc",
+        year: "2026"
+    },
+    {
+        id: 14,
+        title: "Artwork 14",
+        imageUrl: "images/nithar silly.png",
+        description: "this is a drawing",
+        category: "lab oc",
+        year: "2024"
+    },
+    {
+        id: 15,
+        title: "Artwork 15",
+        imageUrl: "images/wowzers.png",
+        description: "this is a drawing",
+        category: "misc",
+        year: "2024"
+    },
+    {
+        id: 16,
+        title: "Artwork 16",
+        imageUrl: "images/cosmos.png",
+        description: "this is a drawing",
+        category: "misc",
+        year: "2023"
     }
 ];
--- data/projectData.js
+++ data/projectData.js
@@ -2,22 +2,25 @@ const projectsData = [
     {
         id: 1,
         number: "01",
-        title: "Project 1",
-        description: "placeholder",
-        tags: ["tag 1", "tag 2", "tag 3"]
+        title: "Ranked or Rated",
+        description: "A Java application developed as part of CPSC 210, focusing on object-oriented design and modular architecture. The project follows the MVC pattern, supports data persistence using JSON, and includes unit testing with JUnit.",
+        tags: ["Java", "Data Persistence", "Software Design"],
+        link: "https://github.com/codepointpen/CPSC-210-Project"
     },
     {
         id: 2,
         number: "02",
-        title: "Project 2",
-        description: "placeholder",
-        tags: ["tag 1", "tag 1", "tag 2"]
+        title: "A Cozy Inn",
+        description: "A UNITY RP prototype focusing on object/NPC interaction. Utilized collision detection, Cinemachine virtual camera, Rigidbody2D, Animator Blend Trees, and created hand-drawn pixel art assets for the Player/NPC sprites and tiles using tilemaps.",
+        tags: ["C#", "UNITY", "Pixel Art", "User Interaction"],
+        link: "https://github.com/codepointpen/A-Cozy-Inn"
     },
     {
         id: 3,
         number: "03",
-        title: "Project 3",
-        description: "placeholder",
-        tags: ["tag 3", "tag 3", "tag 3"]
+        title: "Sign Language Translator",
+        description: "In progress! Take in either a video or live footage and translate the alphabet from American sign language to English text using machine learning models trained off user-created gestures and SQL databases.",
+        tags: ["MySQL", "Machine Learning", "Motion Sensor Detector"],
+        link: "https://github.com/codepointpen"
     }
 ];
